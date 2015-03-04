abeffefc2de9406a454c012fa15a00dc616fd293
Jensus <jensusberlin@gmail.com>
2015-03-04 14:33:19 +0100

    Range Support.

--- js/main.js
+++ js/main.js
@@ -5,13 +5,20 @@ $(document).ready(function(){
 function decreaseValue(caller, number)
 {
 	var input = $(caller).parent().parent().find('input[type="text"]');
-	input.val(parseInt(input.val()) - 5).trigger('change');
+	input.val(parseInt(input.val()) - number).trigger('change');
 }
 
 function increaseValue(caller, number)
 {
 	var input = $(caller).parent().parent().find('input[type="text"]');
-	input.val(parseInt(input.val()) + 5).trigger('change');
+	input.val(parseInt(input.val()) + number).trigger('change');
+}
+
+function correctValue(caller)
+{
+	var that = $(caller);
+	if (parseInt(that.val()) > 100) that.val(100);
+	if (parseInt(that.val()) < 0) that.val(0);
 }
 
 function sendValue(caller)
